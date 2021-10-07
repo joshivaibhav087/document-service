8ffa327c7bd4388732cf62e8b6cfebe051deb68b
Vaibhav Joshi <joshidvaibhav@gmail.com>
2021-10-08 00:04:02 +0530

    db changed

--- src/test/kotlin/com/delta/document/DocumentServiceApplicationTests.kt
+++ src/test/kotlin/com/delta/document/DocumentServiceApplicationTests.kt
@@ -1,20 +1,14 @@
 package com.delta.document
 
 import com.delta.document.dao.PolicyHolderDao
-import com.delta.document.model.PolicyHolder
+
 import com.delta.document.repository.PolicyHolderRepository
 import com.delta.document.service.PolicyService
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.Test
-import org.mockito.Mockito
+
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
-import org.springframework.boot.test.web.client.TestRestTemplate
-import org.springframework.http.HttpEntity
-import org.springframework.http.HttpMethod
-import org.springframework.http.HttpStatus
-import java.net.URI
+
 
 import java.util.*
 
